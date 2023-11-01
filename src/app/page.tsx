6acd0206c2531bff67f7bb9c42725e8958be47e9
aminul-islam01101 <aminul.islam01101@gmail.com>
2023-11-01 13:06:39 +0600

    github setup with linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="">
         <Button variant="outline" size="lg">
-          hellos
+          hello linting
         </Button>
       </h1>
     </main>
