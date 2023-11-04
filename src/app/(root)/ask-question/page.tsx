32f9e0b1457b8079922b10051a1bc322108680e2
aminul-islam01101 <aminul.islam01101@gmail.com>
2023-11-04 12:16:43 +0600

    web hooks added

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+import Question from '@/components/forms/Question';
+import { getUserById } from '@/lib/actions/user.action';
+
+const Page = async () => {
+  const { userId } = auth();
+
+  if (!userId) redirect('/sign-in');
+
+  const mongoUser = await getUserById({ userId });
+  console.log('🌼 🔥🔥 file: page.tsx:13 🔥🔥 Page 🔥🔥 mongoUser🌼', mongoUser);
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+      </div>
+    </div>
+  );
 };
 
-export default AskQuestion;
+export default Page;
