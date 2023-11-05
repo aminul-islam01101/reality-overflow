f2881d0b6bea3af26b5ae5da0042811a4ef6c56a
aminul-islam01101 <aminul.islam01101@gmail.com>
2023-11-05 15:38:08 +0600

    ADDED: view counts and tag details

--- src/app/(root)/collection/page.tsx
+++ src/app/(root)/collection/page.tsx
@@ -1,29 +1,40 @@
+import { auth } from '@clerk/nextjs';
+
 import QuestionCard from '@/components/cards/QuestionCard';
+import Filter from '@/components/shared/Filter';
 import NoResult from '@/components/shared/NoResult';
 import Pagination from '@/components/shared/Pagination';
 import LocalSearchbar from '@/components/shared/search/LocalSearchbar';
-import { getQuestionsByTagId } from '@/lib/actions/tag.actions';
-import { URLProps } from '@/types';
+import { QuestionFilters } from '@/constants/filters';
+import { getSavedQuestions } from '@/lib/actions/user.action';
+import { SearchParamsProps } from '@/types';
 
-const TagDetails = async ({ params, searchParams }: URLProps) => {
-  const result = await getQuestionsByTagId({
-    tagId: params.id,
-    page: searchParams.page ? +searchParams.page : 1,
+export default async function Home({ searchParams }: SearchParamsProps) {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
     searchQuery: searchParams.q,
+    filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
   });
 
   return (
     <>
-      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
 
-      <div className="mt-11 w-full">
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearchbar
-          route={`/tags/${params.id}`}
+          route="/"
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
-          placeholder="Search tag questions"
+          placeholder="Search for questions"
           otherClasses="flex-1"
         />
+
+        <Filter filters={QuestionFilters} otherClasses="min-h-[56px] sm:min-w-[170px]" />
       </div>
 
       <div className="mt-10 flex w-full flex-col gap-6">
@@ -43,7 +54,7 @@ const TagDetails = async ({ params, searchParams }: URLProps) => {
           ))
         ) : (
           <NoResult
-            title="There’s no tag question saved to show"
+            title="There’s no question saved to show"
             description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
             link="/ask-question"
             linkTitle="Ask a Question"
@@ -59,6 +70,4 @@ const TagDetails = async ({ params, searchParams }: URLProps) => {
       </div>
     </>
   );
-};
-
-export default TagDetails;
+}
